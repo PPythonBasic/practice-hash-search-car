ee42759c533ff08fc43f1b8f050921c57933e69f
watchakorn-18k <74919942+watchakorn-18k@users.noreply.github.com>
2023-12-24 00:37:26 +0700

    chore: add code

---
+++ main.py
@@ -0,0 +1,23 @@
+# ข้อมูลความเร็วรถ
+speed_list = [100, 120, 140, 160, 180, 200, 220, 240, 260, 280]
+
+# ช่วงความเร็วที่ต้องการ
+min_speed = 210
+max_speed = 230
+
+# ข้อมูลรถตามความเร็ว
+car_name_list = ["Camry", "Corolla", "Prius", "Rav4","Supra", "Tundra", "Yaris", "Altima", "Sentra", "Titan"]
+
+# แปลงเป็น hash
+min_speed_hash = hash(min_speed)
+max_speed_hash = ..........
+
+
+
+
+
+
+
+
+
+# ผลลัพธ์ที่ต้องการ Supra 180 km/h
